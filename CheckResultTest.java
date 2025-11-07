009f77cd1592dbb0beb602b42b1277303bf95b29
Tresure <ttrotter@xula.edu>
2025-11-07 13:16:33 -0600

    Added Tests 3 and 4

--- CheckResultTest.java
+++ CheckResultTest.java
@@ -19,3 +19,18 @@ public class CheckResultTest {
         assertEquals(0, cr.getMessages().size());
     }
 
+// 3. adding one message stores it
+    @Test
+    public void addMessage_storesMessage() {
+        CheckResult cr = new CheckResult();
+        cr.addMessage("Missing period.");
+        assertEquals("Missing period.", cr.getMessages().get(0));
+    }
+
+    // 4. adding one message makes it fail
+    @Test
+    public void addMessage_setsPassedFalse() {
+        CheckResult cr = new CheckResult();
+        cr.addMessage("Missing subject.");
+        assertFalse(cr.isPassed());
+    }
